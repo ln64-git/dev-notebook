64bb6c41e4857eaad70960f12d95944ecb44c38e
Luke <lucian.io@outlook.com>
2024-06-11 05:30:34 -0400

    test actors

--- Python/01-autogen/main.py
+++ Python/01-autogen/main.py
@@ -4,9 +4,20 @@ from autogen import UserProxyAgent
 from agents.assistant import create_assistant
 from config.config import get_config
 
+from agents_test.manager import create_manager_agent
+from agents_test.architect import create_software_architect_agent
+from agents_test.dev_senior import create_senior_developer_agent
+from agents_test.dev_junior import create_junior_developer_agent
+
 def main():
     config = get_config()
+
     assistant = create_assistant(config)
+
+    # manager = create_manager_agent(config)
+    # architect = create_software_architect_agent(config)
+    # dev_senior = create_senior_developer_agent(config)
+    # dev_junior = create_junior_developer_agent(config)
 
     user_proxy = UserProxyAgent(
         "user_proxy",
